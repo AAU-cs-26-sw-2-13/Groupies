ee0a31e083cdaddb5fa44ed1b7d185bed6c8fce2
Mikkel Dissing <mdissi25@student.aau.dk>
2026-03-13 09:36:49 +0100

    Ammended queries and added dynamic html page generation support

--- server/router.js
+++ server/router.js
@@ -7,12 +7,34 @@ import { parseJSON, setSessionCookie, getSession } from "./routerHelpers.js"
 import { getAllUsers } from "./serverQueries.js";
 export { createResponse }
 
-async function createResponse(req, res) {
-    let baseURL = 'http://' + req.headers.host + "/";    //https://github.com/nodejs/node/issues/12682
-    let url = new URL(req.url, baseURL);
+async function createResponse(req, res){
+    let baseURL = 'http://' + req.headers.host+"/";    //https://github.com/nodejs/node/issues/12682
+    let url=new URL(req.url, baseURL);
+    
+    switch(req.method){
+        case "POST":{
+            let pathElements = url.pathname.split("/")
+            switch (pathElements[1]){
+                case "":{
+                    //Load discovery feed
+                    let data = ""
+                    req.on('data', chunk => {
+                        data += chunk.toString()
+                    })
+                    req.on('end', ()=>{
+                        let jsonData = JSON.parse(data)
+                        if (jsonData.sessionId === "empty"){
+                            if(jsonData.query = "users"){
+                                queryResponse(res, getAllUsers)
+                            }
+                        }
+                    })
+                }
+            }
+            break
 
-    switch (req.method) {
-                case "GET": {
+        }
+        case "GET": {
             let pathElements = url.pathname.split("/")
             //Routing to different paths
             switch (pathElements[1]) {
