99f69e52ff32a869b8cb42300bf6197f42e84c92
Mathias K <116955176+MathiasK-Software@users.noreply.github.com>
2026-03-13 11:42:59 +0100

    Resolved duplicates and reordered url cases in router

--- server/router.js
+++ server/router.js
@@ -1,36 +1,34 @@
 import bcrypt from "bcrypt"; //for password hashing purposes
 import crypto from "node:crypto";
 
-import bcrypt from "bcrypt"; //for password hashing purposes
-import crypto from "node:crypto";
-
 import path, { relative } from "path"
 import { fileResponse, queryResponse } from "./server.js";
 import { parseJSON, setSessionCookie, getSession } from "./routerHelpers.js"
-import { parseJSON, setSessionCookie, getSession } from "./routerHelpers.js"
 import { getAllUsers } from "./serverQueries.js";
 export { createResponse }
-export { createResponse }
-
-
-
 
 async function createResponse(req, res) {
     let baseURL = 'http://' + req.headers.host + "/";    //https://github.com/nodejs/node/issues/12682
     let url = new URL(req.url, baseURL);
 
     switch (req.method) {
-        case "POST": {
-
-async function createResponse(req, res) {
-    let baseURL = 'http://' + req.headers.host + "/";    //https://github.com/nodejs/node/issues/12682
-    let url = new URL(req.url, baseURL);
-
-    switch (req.method) {
-        case "POST": {
+                case "GET": {
             let pathElements = url.pathname.split("/")
+            //Routing to different paths
             switch (pathElements[1]) {
                 case "": {
+                    fileResponse(res, "html/index.html")
+                    break;
+                }
+                default: {
+                    fileResponse(res, url.pathname)
+                    break;
+                }
+            }
+        }
+        case "POST": {
+            let pathElements = url.pathname.split("/")
+            
             switch (pathElements[1]) {
                 case "": {
                     //Load discovery feed
@@ -38,11 +36,9 @@ async function createResponse(req, res) {
                     req.on('data', chunk => {
                         data += chunk.toString()
                     })
-                    req.on('end', () => {
-                    req.on('end', () => {
+                 req.on('end', () => {
                         let jsonData = JSON.parse(data)
-                        if (jsonData.sessionId === "empty") {
-                            if (jsonData.query === "users") {
+
                         if (jsonData.sessionId === "empty") {
                             if (jsonData.query === "users") {
                                 queryResponse(res, getAllUsers)
@@ -72,7 +68,7 @@ async function createResponse(req, res) {
                                             return res.end(JSON.stringify({ error: "Username already taken!" }));
                                         }
                                         /* If username and password received and username unique, hash a password and query insert user */
-                                        const hash = bcrypt.hash(password, 12);
+                                        const hash = await bcrypt.hash(password, 12);
                                         await query("INSERT INTO users (username, password_hash, name)", [username, hash, name || null]);
 
                                         console.log(`✓ User registered: ${username}`); //Debug log
@@ -154,7 +150,6 @@ async function createResponse(req, res) {
                                 //handle no preference received
                             }
 
-                            query =
 
                                 res.writeHead(200, { "Content-Type": "application/json" });
                             return res.end(JSON.stringify({
@@ -167,21 +162,6 @@ async function createResponse(req, res) {
                     }
                 }
                     break;
-            }
-        }
-        case "GET": {
-            let pathElements = url.pathname.split("/")
-            //Routing to different paths
-            switch (pathElements[1]) {
-            switch (pathElements[1]) {
-                case "": {
-                    fileResponse(res, "html/index.html")
-                    break;
-                }
-                default: {
-                    fileResponse(res, url.pathname)
-                    break;
-                }
             }
         }
     }
