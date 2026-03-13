68005633734bb0cfab1b647b79f84b53fdbbbcfe
MaybeMati <jagimateusz@gmail.com>
2026-03-13 11:15:24 +0100

    Users table more fitting to drawsql

--- database/inserts/insert_user.sql
+++ database/inserts/insert_user.sql
@@ -1,2 +1,5 @@
-INSERT INTO users(username,name, password_hash)
-VALUES("Mikkel2","Mikkel Dissing2","Test")
+INSERT INTO users(name_first,name_last, email, password_hash, country,age,bio, picture)
+VALUES("Mikkel", "Dissing", "MikkelDissing@email.com","Test", "DK", "11", NULL, NULL);
+
+
+
