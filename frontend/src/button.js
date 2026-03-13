2d001a816cbfb31801a1f9eef168bf3fbc8b5692
Ruben <Nordsoe2003@proton.me>
2026-03-13 11:43:19 +0100

    html register

--- frontend/src/button.js
+++ frontend/src/button.js
@@ -1,5 +1,7 @@
+//creates button
 let btnClick = document.querySelector("#register");
 
+//listens for register button press
 btnClick.addEventListener("click", () => {
     window.location.href = "/html/register.html"
 });
---
+++ frontend/src/register.js
@@ -0,0 +1,23 @@
+const form1 = document.querySelector("#stepOne")
+
+async function sendData() {
+    const fromData = new FormData(form1);
+
+    try {
+        const response = await fetch("http://localhost:3000/api/register", {
+            method: "POST",
+            body: fromData,
+        });
+        const data = await response.json();
+
+        console.log(data);
+    } catch(e) {
+        console.error(e);
+    }
+}
+
+//listens for submit
+form1.addEventListener("submit", (event) => {
+    event.preventDefault();
+    sendData();
+});
