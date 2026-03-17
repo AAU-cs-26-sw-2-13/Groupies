35142b029f0bbc8f749a1e1c990443acb082e97a
vdalg <victordalg@gmail.com>
2026-03-17 09:35:18 +0100

    Mocked data for users and preferences, depending on 'TESTING_MOCKDATA' .env var

--- database/migrations/01-03_user_prefs.sql
+++ database/migrations/01-03_user_prefs.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS user_prefs (
   id INT AUTO_INCREMENT PRIMARY KEY,
   user_id INT NOT NULL,
   preference_id VARCHAR(50) NOT NULL,
-  preference_value BINARY NOT NULL DEFAULT 0
+  preference_value BINARY NOT NULL DEFAULT 0,
   
   FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
   FOREIGN KEY (preference_id) REFERENCES preferences(preference_id) ON DELETE CASCADE
--- testing/mockData.js
+++ testing/mockData.js
@@ -1,8 +1,30 @@
 import { query } from "../database/pool.js";
 import { faker } from '@faker-js/faker';
-export async function mockUsers(amount) {
+
+export async function mockUserPreferences(amount){
+    let result = await query(`SELECT * FROM preferences`);
+    let n = result.length;
+
+    let mockUserPreferences = [];
+    for (let i=1; i <= amount; i++) {
+        for (let j=0; j < n; j++) {
+            if (Math.random < 0.5) { continue; }
+            let preference = [i, result[j][`preference_id`], 1];
+            mockUserPreferences.push(preference);
+        }
+    }
+    
+    await query(
+        `INSERT INTO user_prefs (user_id, preference_id, preference_value) VALUES ?`,
+        [mockUserPreferences]
+    );
+    
+    console.log(`✓ Mocked preferences for ${amount} of users`);
+}
+
+export async function mockUsers(amount){
     let mockUsers = [];
-    for (let i = 0; i < amount; i++) {
+    for (let i=0; i < amount; i++) {
         let gender = faker.person.sexType()
         let user = [
             faker.person.firstName(gender), 
@@ -24,4 +46,6 @@ export async function mockUsers(amount) {
     );
 
     console.log(`✓ Mocked ${amount} of users`);
+    await mockUserPreferences(amount);
 }
+
