6e6f35ecc1deac9f919cca04b7f233097d63889b
Mikkel Dissing <mdissi25@student.aau.dk>
2026-03-17 11:41:32 +0100

    Added new queries. Added session finder. Added tags for groups

---
+++ database/inserts/insert_group_pref.sql
@@ -0,0 +1,3 @@
+INSERT INTO group_tags(group_id, tag_id, tag_value)
+VALUES(1, "History", 1)
+
--- database/inserts/insert_user.sql
+++ database/inserts/insert_user.sql
@@ -1,5 +1,5 @@
 INSERT INTO users(name_first,name_last, email, password_hash, country, gender ,age,bio, picture)
-VALUES("Mikkel3", "Dangeler", "MikkelDissing@email.com","Test", "US", "male","11", NULL, NULL);
+VALUES("Mikkel", "E", "MikkelDissing@email.com","Test", "US", "male","11", NULL, NULL);
 
 
 
