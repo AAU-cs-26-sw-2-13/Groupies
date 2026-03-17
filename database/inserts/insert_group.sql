5cc9db573edd50999b928cf458726958989dd8f9
Mikkel Dissing <mdissi25@student.aau.dk>
2026-03-17 12:02:17 +0100

    fixed group tags empty space

--- database/inserts/insert_group.sql
+++ database/inserts/insert_group.sql
@@ -1,2 +1,2 @@
 INSERT INTO `groups`(host_user_id, title, destination, about, date_start_at, date_end_at, picture)
-VALUES(2,"USA TUR TIL FORÅRET","tokyuo", "Sjov tur", NULL, NULL, NULL)
+VALUES(1,"USA TUR TIL FORÅRET","tokyuo", "Sjov tur", NULL, NULL, NULL)
