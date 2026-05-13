116c41fc8511d1d3bb0577fbfbb7acef80a40062
Mathias K <116955176+MathiasK-Software@users.noreply.github.com>
2026-05-13 12:50:10 +0200

    AAA comments instead

--- testing/test/databaseIntegrations.test.js
+++ testing/test/databaseIntegrations.test.js
@@ -17,18 +17,17 @@ after(() => { pool.end(); });
 /* -------------- Unit database integration tests (x) -------------- */
 
 /* (test 1) Make a login with credentials for existing user (admin user) */
-
 test('loginUser Function: Succesful login from admin credentials', async () => {
-  // SETUP: Test credentials (the admin account always exists in the database)
+  // Arrange: Test credentials (the admin account always exists in the database)
   const TEST_EMAIL = "admin", TEST_PASS = "123", TEST_ID = 1;
+  await query("DELETE FROM sessions WHERE user_id = ?", [TEST_ID]);
 
-  // Execute: run the login function
+  // Act: run the login function
   const mockObject = await loginTestAccount(TEST_EMAIL, TEST_PASS, TEST_ID);
 
-  // Verify: response.status = 200 and the session exists in DB
+  // Assert: response.status = 200 and then check the database was altered correctly
   assert.strictEqual(mockObject.responseData.status, 200, "Status code must be 200");
-
-  const [session] = await query("SELECT * FROM sessions WHERE user_id = ?", [TEST_ID]);
+  const [session] = await query("SELECT * FROM sessions WHERE user_id = ?", [TEST_ID]); //assertion by database state after test
   assert.ok(session, "Must exist a session in database");
   assert.strictEqual(session.user_id, TEST_ID, "... which belongs to correct user");
 });
@@ -37,13 +36,13 @@ test('loginUser Function: Succesful login from admin credentials', async () => {
 const ACTIVE_USER = 1, TARGET_USER = 2;
 
 test('followUser Database Integration Test', async () => {
-  // Setup: the database to ensure the data to insert does not already exist
+  // Arrange: the database to ensure the data to insert does not already exist
   await query("DELETE FROM user_relations WHERE user_id = ? AND target_user_id = ?", [ACTIVE_USER, TARGET_USER]);
 
-  // Execute: the server entry for the follow a user endpoint on frontend
+  // Act: the server entry for the follow a user endpoint on frontend
   const queryRes = await queryFollowUser(TARGET_USER, ACTIVE_USER);
 
-  // Verify: the row now exists in db
+  // Assert: the row now exists in db
   const rows = await query("SELECT * FROM user_relations WHERE user_id = ? AND target_user_id = ?", [ACTIVE_USER, TARGET_USER])
   assert.strictEqual(rows.length, 1, "The row should exist in the database");
 });
@@ -51,18 +50,17 @@ test('followUser Database Integration Test', async () => {
 /* (test 3) set the preferences for a user with existing preferences (the admin user)*/
 test('setUserPreferences updates the user preferences for test user to an array of prefs', async () => {
   const TEST_ID = 1, testPrefList = ["Sightseeing", "Nightlife", "Beaches"]
-  //Setup: the values to insert, which are different from the default migrated (skiing) for the admin user
+  //Arrange: the values to insert, which are different from the default migrated (skiing) for the admin user and mock a req/res object
   const requestBody = {
     user_id: TEST_ID,
     preferenceList: testPrefList
   }
-  //mock the HTTP request and result object
   const mockObject = mockReqRes(requestBody);
 
-  //Execute: "send the request" to the function
+  //Act: "send the request" to the function
   await setUserPreferences(mockObject.req, mockObject.res);
 
-  //Verify: the status code is 200 and the preferencelist set for the user is now equal to the test list set
+  //Assert: the status code is 200 and the preferencelist set for the user is now equal to the test list set
   assert.strictEqual(mockObject.responseData.status, 200, "Status code must be 200");
 
   const session = await query("SELECT preference_id FROM user_prefs WHERE user_id = ?", [TEST_ID])
@@ -103,11 +101,13 @@ function mockReqRes(requestObject) {
 async function loginTestAccount(TEST_EMAIL, TEST_PASS, TEST_ID) {
   await query("DELETE FROM sessions WHERE user_id = ?", [TEST_ID]);
 
-  // Setup: Mock request as a readable stream (Readable.from), like a real request
+  // Arrange: Mock request as a readable stream (Readable.from), like a real request
   const mockObject = mockReqRes({ email: TEST_EMAIL, password: TEST_PASS });
 
-  // Execute: send the mocked req res to loginUser function
+  // Act: send the mocked req res to loginUser function
   await loginUser(mockObject.req, mockObject.res);
+
+  // Assert: that the result was written by the loginUser function
   assert.ok(mockObject.res, 'The login setup for editUser() test was unsuccesful');
 
   return mockObject;
